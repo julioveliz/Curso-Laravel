2d9ef1e588f8e4a4f481c9806153edb6bebaeda4
julio veliz <julio.cesar.veliz.s@gmail.com>
2019-07-08 13:35:07 -0400

    Primer Commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'InicioController@index')->name('inicio');
+Route::get('seguridad/login', 'Seguridad\LoginController@index')->name('login');
+Route::post('seguridad/login', 'Seguridad\LoginController@login')->name('login_post');
+Route::get('seguridad/logout', 'Seguridad\LoginController@logout')->name('logout');
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'superadmin']], function () {
+    Route::get('', 'AdminController@index');
+    Route::get('permiso', 'PermisoController@index')->name('permiso');
+    Route::get('permiso/crear', 'PermisoController@crear')->name('crear_permiso');
+    /*RUTAS DEL MENU*/
+    Route::get('menu', 'MenuController@index')->name('menu');
+    Route::get('menu/crear', 'MenuController@crear')->name('crear_menu');
+    Route::post('menu', 'MenuController@guardar')->name('guardar_menu');
+    Route::post('menu/guardar-orden', 'MenuController@guardarOrden')->name('guardar_orden');
+    /*RUTAS ROL*/
+    Route::get('rol', 'RolController@index')->name('rol');
+    Route::get('rol/crear', 'RolController@crear')->name('crear_rol');
+    Route::post('rol', 'RolController@guardar')->name('guardar_rol');
+    Route::get('rol/{id}/editar', 'RolController@editar')->name('editar_rol');
+    Route::put('rol/{id}', 'RolController@actualizar')->name('actualizar_rol');
+    Route::delete('rol/{id}', 'RolController@eliminar')->name('eliminar_rol');
+    /*RUTAS MENU_ROL*/
+    Route::get('menu-rol', 'MenuRolController@index')->name('menu_rol');
+    Route::post('menu-rol', 'MenuRolController@guardar')->name('guardar_menu_rol');
+});
